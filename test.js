c6c81993196377eba5c33421438551446768a2a1
Multi Culti Home Server <mchs@mchs.lan.asjervis.eu>
2014-05-20 10:09:16 +0200

    - Fixed '{' separation from ')'  - Moved testapp/controller to separate test.js

---
+++ test.js
@@ -0,0 +1,19 @@
+// For testing only
+var app = angular.module('testapp', []);
+app.controller('testctl', function($scope) {
+	$scope.characters = [
+		new Arcini.Model.Character('Jonathan', [ 3, 0, 6, 1, 0 ]),
+		new Arcini.Model.Character('Cartha', [ 3, 0, 1, 0, 6 ]),
+		new Arcini.Model.Character('Kiara', [ 4, 6, 0, 0, 0 ]),
+		new Arcini.Model.Character('Goblin God', [ 11, 0, 7, 7, 0 ]),
+
+	];
+
+	$scope.deleteCharacter = function(idx) {
+		$scope.characters.splice(idx, 1);
+	};
+
+	$scope.addCharacter = function() {
+		$scope.characters.push(new Arcini.Model.Character());
+	};
+});
