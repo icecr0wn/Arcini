cd443d55bd94509436a9b2881361c03315484e33
icecr0wn <aj.visser@gmail.com>
2014-06-07 09:51:45 +0200

    Moved deity definition to Arcini.Data.Deity()

--- arcini.js
+++ arcini.js
@@ -367,8 +367,33 @@ var Arcini = (function() {
 						attributes: attributes,
 						resistances: resistances
 					};
-				}
+				},
 			};
-		}())
+		}()),
+		Data: (function() {
+			var Deity = function() {
+				var deities = [
+					{ name: '<choose>', value: new Arcini.Model.Deity('<choose>', [ 0, 0, 0, 0, 0 ], [ 0, 0, 0, 0, 0 ]) },
+					{ name: 'Almarea', value: new Arcini.Model.Deity('Almarea', [ 3, 3, 0, 0, 0 ], [ 0, 2, 0, 0, 0 ]) },
+					{ name: 'Baramethor', value: new Arcini.Model.Deity('Baramaethor', [ 3, 0, 0, 3, 0 ], [ 0, 0, 0, 2, 0 ]) },
+					{ name: 'Eferhilda', value: new Arcini.Model.Deity('Eferhilda', [ 3, 0, 0, 0, 3 ], [ 0, 0, 0, 0, 2 ]) },
+					{ name: 'Herion', value: new Arcini.Model.Deity('Herion', [ 3, 0, 2, 0, 1 ], [ 0, 0, 1, 0, 1 ]) },
+					{ name: 'Jade', value: new Arcini.Model.Deity('Jade', [ 3, 1, 0, 2, 0 ], [ 0, 1, 0, 1, 0 ]) },
+					{ name: 'Khalon', value: new Arcini.Model.Deity('Khalon', [ 3, 0, 3, 0, 0 ], [ 0, 0, 2, 0, 0 ]) },
+					{ name: 'Lovisa', value: new Arcini.Model.Deity('Lovisa', [ 3, 2, 0, 0, 1 ], [ 0, 1, 0, 0, 1 ]) },
+					{ name: 'Naerdiel', value: new Arcini.Model.Deity('Naerdiel', [ 5, 1, 0, 0, 0 ], [ 2, 0, 0, 0, 0 ]) },
+					{ name: 'Zebulon', value: new Arcini.Model.Deity('Zebulon', [ 3, 0, 2, 1, 0 ], [ 0, 0, 1, 1, 0 ]) }
+				];
+
+				return {
+					deities: deities
+				};						
+			};
+			
+			return {
+				Deity: Deity
+			};
+		}()),
+
 	};
 }());
