19239cefd3ddfb18da081728f5571f6ccba99e8f
icecr0wn <aj.visser@gmail.com>
2014-06-11 11:58:27 +0200

    Converted test.js to arcini with services/factories/controllers

---
+++ ctrl_arcini.js
@@ -0,0 +1,80 @@
+var arcini = angular.module('Arcini.Module.Arcini', []);
+
+arcini.factory('Arcini.Factory.Character', function() {
+	var create = function(characterName, attributes, deity) {
+		if (!characterName) {
+			characterName = '<choose name>'
+		};
+
+		if (!attributes) {
+			attributes = [ 0, 0, 0, 0, 0 ];
+		};
+
+		if (!deity) {
+			deity = 0;
+		}
+
+		return new Arcini.Model.Character(characterName, [ 0, 0, 0, 0, 0 ], 0);
+	};
+
+	return {
+		create: create
+	};
+});
+
+arcini.service('Arcini.Service.Character', [ 'Arcini.Factory.Character', function(factory) {
+	var characters = [];
+
+	var add = function() {
+		characters.push(factory.create());
+	};
+
+	var remove = function(index) {
+		characters.splice(index, 1);
+	};
+
+	var get = function(index) {
+		return characters[index];
+	};
+
+	var list = function() {
+		return characters;
+	};
+
+	return {
+		add: add,
+		remove: remove,
+		get: get,
+		list: list
+	};
+}]);
+
+arcini.controller('Arcini.Controller.Creator', [ '$scope', 'Arcini.Service.Character', function($scope, character) {
+	$scope.characters = character.list();
+
+	$scope.deities = Arcini.Constants.Deities().List;
+
+	$scope.addCharacter = function() {
+		character.add();
+	};
+
+	$scope.deleteCharacter = function(index) {
+		console.log('About to remove character[' + index + ']');
+		character.remove(index);
+	};
+}]);
+
+arcini.directive('integer', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, ele, attr, ctrl) {
+            ctrl.$parsers.unshift(function(viewValue) {
+            	if (!viewValue) {
+            		return 0;
+            	}
+                return parseInt(viewValue);
+            });
+        }
+    };
+});
+
