89205ff7e3321ea934e765352cc09ff6e9e97762
icecr0wn <aj.visser@gmail.com>
2014-08-08 13:55:09 +0200

    Added tests for common/resource

---
+++ go/src/common/resource/resource_test.go
@@ -0,0 +1,30 @@
+package resource
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+type Dummy struct {
+	A int    `json:"a"`
+	B string `json:"b"`
+	C []int  `json:"c"`
+}
+
+func Test_GetFormattedJson(t *testing.T) {
+	expected := "{\n\t\"a\": 7,\n\t\"b\": \"Test\",\n\t\"c\": [\n\t\t2,\n\t\t5,\n\t\t9\n\t]\n}"
+	dummy := Dummy{7, "Test", []int{2, 5, 9}}
+
+	data, error := json.Marshal(dummy)
+	if nil != error {
+		t.Error("Failed to parse 'type Dummy struct' to JSON.")
+	}
+
+	fmt.Printf(GetFormattedJson(data) + "\n")
+	fmt.Printf(expected + "\n")
+
+	if expected != GetFormattedJson(data) {
+		t.Error("Failed to retrieve correct formatted JSON.")
+	}
+}
