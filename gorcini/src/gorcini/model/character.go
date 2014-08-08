f9a99472e048019567b08eb3fa469068def3e3ca
icecr0wn <aj.visser@gmail.com>
2014-08-08 09:22:56 +0200

    Removed gorcini in preparation for new structure

--- gorcini/src/gorcini/model/character.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type Character struct {
-	Name            string `json:"name"`
-	BaseAttributes  []int  `json:"base"`
-	SpentAttributes []int  `json:"spent"`
-	ChosenDeity     Deity  `json:"deity"`
-}
--- gorcini/src/gorcini/model/deity.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type Deity struct {
-	Name        string `json:"name"`
-	Title       string `json:"title"`
-	Attributes  []int  `json:"attributes"`
-	Resistances []int  `json:"resistances"`
-}
