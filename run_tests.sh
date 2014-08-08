351c79293eac648dbfdbabdae0cbc38afb25ea0e
icecr0wn <aj.visser@gmail.com>
2014-08-08 14:27:08 +0200

    Fixed function call in run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -5,9 +5,9 @@ function runTest()
   go test -v -cover ${name}
 }
 
-runTest("common")
-runTest("common/resource")
-runTest("common/server")
-runTest("gorcini")
-runTest("gorcini/model")
-runTest("gorcini/resource")
+runTest "common"
+runTest "common/resource"
+runTest "common/server"
+runTest "gorcini"
+runTest "gorcini/model"
+runTest "gorcini/resource"
